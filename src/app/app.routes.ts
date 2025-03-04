6897481f8d714e3a888c7262673fb1267a922d79
alexandra.castano <alexandra.castanotrochez.globant@kantar.com>
2025-03-04 14:28:47 -0500

    routing for characters and character id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CharacterComponent } from './characters/character/character.component';
+import { CharactersComponent } from './characters/characters.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'characters', component: CharactersComponent },
+    { path: 'characters/:id', component: CharacterComponent },
+];
